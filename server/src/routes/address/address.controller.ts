071483f3252b4efe7996a69e223c488b5ff5a448
Le Anh Duc <ducvui2003@gmail.com>
2025-04-03 10:36:15 +0700

    feat: resolves #31

--- server/src/routes/address/address.controller.ts
+++ server/src/routes/address/address.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AddressService } from './address.service';
 
-@Controller('address')
+@Controller('/api/v1/address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
+
+  @Get('/province')
+  public getProvince() {
+    return this.addressService.getAddress('CITY');
+  }
+
+  @Get('/district/:province_id')
+  public getDistrict(@Param('province_id') provinceId: number) {
+    return this.addressService.getAddress('DISTRICT', provinceId);
+  }
+
+  @Get('/ward/:district_id')
+  public getWard(@Param('district_id') districtId: number) {
+    return this.addressService.getAddress('WARD', districtId);
+  }
 }
