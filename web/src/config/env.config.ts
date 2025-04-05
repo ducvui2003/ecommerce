3b40fa0cc0efbfc57222161b9169cdfa77ef3cab
Le Anh Duc <ducvui2003@gmail.com>
2025-04-05 16:32:09 +0700

    feat: resolves #40

--- web/src/config/env.config.ts
+++ web/src/config/env.config.ts
@@ -8,6 +8,7 @@ const configSchema = z.object({
   NEXT_PUBLIC_FACEBOOK_CLIENT_ID: z.string(),
   NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET: z.string(),
   NEXT_PUBLIC_DEVELOPMENT: z.string(),
+  NEXT_PUBLIC_LOG_CLIENT: z.string().transform((value) => new Boolean(value)),
 });
 
 const configProject = configSchema.safeParse({
@@ -20,10 +21,11 @@ const configProject = configSchema.safeParse({
   NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET:
     process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET,
   NEXT_PUBLIC_DEVELOPMENT: process.env.NEXT_PUBLIC_DEVELOPMENT,
+  NEXT_PUBLIC_LOG_CLIENT: process.env.NEXT_PUBLIC_LOG_CLIENT,
 });
 
 if (!configProject.success) {
-  console.log(configProject.error.issues);
+  console.error(configProject.error.issues);
   throw new Error('Các giá trị khai báo trong file .env không hợp lệ');
 }
 
