3fcc493a19f4d2c3002e0b30a9c633b63250d898
Lê Anh Đức <leanhduc.develop@gmail.com>
2025-04-15 23:33:02 +0700

    Ref/upgrade tailwindcss v4 (#52)
    * feat: create Header Wrapper to reuse layout
    
    * ref: resolves #51
    
    * fix: migration react toast to sonner

--- web/src/components/ProductCard.tsx
+++ web/src/components/ProductCard.tsx
@@ -26,19 +26,19 @@ const ProductCard = ({
         <AspectRatio ratio={9 / 10} className="overflow-hidden">
           <Image src={thumbnail} alt={name} fill className="object-contain" />
         </AspectRatio>
-        <CardTitle className="text-2xl mt-2 text-center">{name}</CardTitle>
-        <span className="price font-bold text-red-500 mt-2 block">
+        <CardTitle className="mt-2 text-center text-2xl">{name}</CardTitle>
+        <span className="price mt-2 block font-bold text-red-500">
           {currency(price)}
         </span>
         {percentSale && (
-          <div className="flex items-center gap-3 mt-2">
+          <div className="mt-2 flex items-center gap-3">
             <span className="text-gray-500 line-through">
               {currency(basePrice)}
             </span>
             <span className="text-red-500">{percentSale}%</span>
           </div>
         )}
-        <div className="flex items-center text-xs mt-2">
+        <div className="mt-2 flex items-center text-xs">
           <ClientIcon
             className="text-yellow-500"
             icon={'material-symbols:star-rounded'}
--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,7 +1,7 @@
-import { toast } from '@/hooks/use-toast';
 import { EntityError } from '@/lib/http';
 import { clsx, type ClassValue } from 'clsx';
 import { UseFormSetError } from 'react-hook-form';
+import { toast } from 'sonner';
 import { twMerge } from 'tailwind-merge';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -31,10 +31,8 @@ export function handleErrorApi({
       });
     });
   } else {
-    toast({
-      title: 'Lỗi',
+    toast.error('Lỗi', {
       description: error?.payload?.error ?? 'Lỗi không xác định',
-      variant: 'destructive',
       duration: duration,
     });
   }
