b9ad9eaba6cb21cb1a0261b94792bc55f973a1b2
ThanhBinh22 <21130291@st.hcmuaf.edu.vn>
2025-05-07 00:11:20 +0700

    refactor(about): clean code in about us page

---
+++ web/src/app/about/about-content.ts
@@ -0,0 +1,31 @@
+import truck from 'public/truck.svg';
+import bag from 'public/bag.svg';
+import support from 'public/support.svg';
+import returnIcon from 'public/return.svg';
+
+export const FEATURES = [
+    {
+      icon: truck,
+      title: 'Giao hàng nhanh & Miễn phí',
+      description:
+        'Đảm bảo giao hàng nhanh chóng, miễn phí cho mọi đơn hàng.',
+    },
+    {
+      icon: bag,
+      title: 'Mua sắm dễ dàng',
+      description:
+        'Giao diện thân thiện, đặt hàng nhanh chóng chỉ với vài bước.',
+    },
+    {
+      icon: support,
+      title: 'Hỗ trợ 24/7',
+      description:
+        'Luôn sẵn sàng giải đáp và hỗ trợ bạn mọi lúc.',
+    },
+    {
+      icon: returnIcon,
+      title: 'Hoàn trả dễ dàng',
+      description:
+        'Chính sách đổi trả linh hoạt, đơn giản và nhanh chóng.',
+    },
+  ];
