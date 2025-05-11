d9cfd33650264c0bcb6354b0843d690bd770a8fd
Lê Anh Đức <leanhduc.develop@gmail.com>
2025-05-11 21:07:31 +0700

    fix: handle refresh token by middleware (#96)

--- server/src/shared/types/jwt.type.ts
+++ server/src/shared/types/jwt.type.ts
@@ -1,10 +1,17 @@
-export class JwtCustomClaims {
+export type JwtCustomClaims = {
   id: number;
   email: string;
-}
+};
 
-export class JwtPayload extends JwtCustomClaims {
+export type JwtPayload = JwtCustomClaims & {
   iat: number;
   exp: number;
   jti: string;
-}
+};
+
+export type JwtData = {
+  token: string;
+  iat: number;
+  exp: number;
+  jti: string;
+};
