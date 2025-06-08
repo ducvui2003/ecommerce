c332310bf4b452f427d2dbaac288e4c13b168acc
Le Anh Duc <ducvui2003@gmail.com>
2025-06-08 22:22:52 +0700

    feat: remove swagger, modifier env

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
-import setupCors from 'src/configs/cors.config';
-import setupSwagger from 'src/configs/swagger.config';
 import envConfig from '@config/env.config';
 import setupExceptionHandling from '@config/exception.config';
-import setUpPipe from '@config/pipe.config';
-import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from 'src/app.module';
+import setupCors from '@config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupExceptionHandling(app);
   setupCors(app);
-  setupSwagger(app);
   await app.listen(envConfig.PORT);
 }
 
