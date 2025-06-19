741e1ddbec071bec1ebda673705639aa5df03f4f
Le Anh Duc <ducvui2003@gmail.com>
2025-06-19 19:06:58 +0700

    feat: statRevenueByTime feat: statRevenueByTimeAndCategory

--- server/src/routes/dashboard/dashboard.schema.ts
+++ server/src/routes/dashboard/dashboard.schema.ts
@@ -23,6 +23,63 @@ const DashboardSchema = z.object({
   }),
 });
 
+const RevenueByTimeRequestSchema = z.object({
+  from: z.coerce.date(),
+  to: z.coerce.date(),
+});
+
+const RevenueByTimeResponseSchema = z.array(
+  z.object({
+    month: z.string(),
+    revenue: z.number(),
+  }),
+);
+
+const RevenueByTimeAndCategoryResponseSchema = z.object({
+  category: z.string(),
+  revenue: z.number(),
+});
+
+const RevenueByTimeAndCategoryRequestSchema = z.object({
+  month: z
+    .string()
+    .transform(Number)
+    .refine((m) => m >= 1 && m <= 12, {
+      message: 'Month must be between 1 and 12',
+    }),
+  year: z
+    .string()
+    .transform(Number)
+    .refine((y) => y >= 2000 && y <= 2100, {
+      message: 'Year must be a valid year',
+    }),
+});
+
+type RevenueByTimeAndCategoryResponseType = z.infer<
+  typeof RevenueByTimeAndCategoryResponseSchema
+>;
+
+type RevenueByTimeAndCategoryRequestType = z.infer<
+  typeof RevenueByTimeAndCategoryRequestSchema
+>;
+
 type DashboardType = z.infer<typeof DashboardSchema>;
-export { DashboardSchema };
-export type { DashboardType };
+
+type RevenueByTimeResponseType = z.infer<typeof RevenueByTimeResponseSchema>;
+
+type RevenueByTimeRequestType = z.infer<typeof RevenueByTimeRequestSchema>;
+
+export {
+  DashboardSchema,
+  RevenueByTimeResponseSchema,
+  RevenueByTimeRequestSchema,
+  RevenueByTimeAndCategoryResponseSchema,
+  RevenueByTimeAndCategoryRequestSchema,
+};
+export type {
+  DashboardType,
+  RevenueByTimeResponseType,
+  RevenueByTimeRequestType,
+  RevenueByTimeAndCategoryResponseType,
+  RevenueByTimeAndCategoryRequestType,
+};
--- server/src/routes/dashboard/dashboard.service.ts
+++ server/src/routes/dashboard/dashboard.service.ts
@@ -1,6 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { DashboardRepository } from '@route/dashboard/dashboard.repository';
-import { DashboardType } from '@route/dashboard/dashboard.schema';
+import {
+  DashboardType,
+  RevenueByTimeAndCategoryResponseType,
+  RevenueByTimeRequestType,
+  RevenueByTimeResponseType,
+} from '@route/dashboard/dashboard.schema';
 
 @Injectable()
 export class DashboardService {
@@ -10,5 +15,16 @@ export class DashboardService {
   ) {}
   getDashboard(): Promise<DashboardType> {
     return this.dashboardRepository.getDashboard();
+  }
+
+  getRevenueByTime(
+    req: RevenueByTimeRequestType,
+  ): Promise<RevenueByTimeResponseType> {
+    return this.dashboardRepository.getRevenueByTime(req.from, req.to);
+  }
+  getRevenueByTimeAndCategory(
+    date: Date,
+  ): Promise<RevenueByTimeAndCategoryResponseType> {
+    return this.dashboardRepository.getRevenueByTimeAndCategory(date);
   }
 }
