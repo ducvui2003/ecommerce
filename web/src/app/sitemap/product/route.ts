826db8cb28330698c267cdb9458278a99502083a
Le Anh Duc <ducvui2003@gmail.com>
2025-06-21 09:51:18 +0700

    format sitemap

--- web/src/app/sitemap/product/route.ts
+++ web/src/app/sitemap/product/route.ts
@@ -26,16 +26,16 @@ export async function GET() {
   }
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${ids
-    .map((item) => {
-      return `<url>
-        <loc>${baseUrl}/product/detail/${item.id}</loc>
-        <lastmod>${new Date(item.createdAt).toISOString()}</lastmod>
-      </url>`;
-    })
-    .join('')}
-</urlset>`;
+                  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+                    ${ids
+                      .map((item) => {
+                        return `<url>
+                          <loc>${baseUrl}/product/detail/${item.id}</loc>
+                          <lastmod>${new Date(item.createdAt).toISOString()}</lastmod>
+                        </url>`;
+                      })
+                      .join('')}
+                  </urlset>`;
   return new NextResponse(xml, {
     headers: {
       'Content-Type': 'application/xml',
